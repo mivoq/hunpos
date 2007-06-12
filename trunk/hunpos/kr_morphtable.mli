83bfeae58e01c1ac02c5c7662048e17b685d2d58
hp <hp@87d68731-0634-0410-9e4a-795b3aa8b831>
2007-06-12 11:22:29 +0000

    rendrakas

--- trunk/hunpos/kr_morphtable.mli
+++
@@ -1,5 +0,0 @@
-type t 
-type parsed_anal = (string * string * string * string) list list
-val load : string -> t
-val tags : t -> string -> string list
-val analyze : t -> string -> parsed_anal * bool
