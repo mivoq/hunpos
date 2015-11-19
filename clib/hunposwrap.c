7be5bcd8dfe5808ababec4d613bcdf588ab894c5
Giulio Paci <giuliopaci@gmail.com>
2015-11-19 23:17:43 +0100

    Updated hunposwrap.c accorging to changes in hunpos.h.

--- clib/hunposwrap.c
+++ clib/hunposwrap.c
@@ -11,16 +11,16 @@
 /* converts an ocaml string list to C++ string vector. In Ocaml a binary predicate context
    is given by a string list
 */
-void string_list_to_C_string_array(value context, char** r) {
+void string_list_to_C_string_array(value context, void* tags, int (*add_tag)(void*,int,const char*)) {
 	CAMLparam1(context);
 	CAMLlocal1(tail);
 
 	/* context is a string list */
 	tail = context;
-  int i = 0;
+	int i = 0;
 	while(tail != Val_emptylist) {
 		char* s = String_val(Field(tail, Tag_cons));
-    r[i++] = s;
+		add_tag(tags, i++, s);
 		tail = Field(tail, 1);
 	}
 	CAMLreturn0;	
@@ -29,7 +29,7 @@ void string_list_to_C_string_array(value context, char** r) {
  
   // one should add some other constructor with default values
 
-hunpos init_hunpos(char* model_file, char* morph_table_file, int max_guessed_tags, int theta)
+Hunpos hunpos_tagger_new(const char* model_file, const char* morph_table_file, int max_guessed_tags, int theta, int* error)
 {
     CAMLparam0();
     if(model_file == NULL) {
@@ -52,7 +52,7 @@ hunpos init_hunpos(char* model_file, char* morph_table_file, int max_guessed_tag
            init_fun = caml_named_value("init_from_files");
      }
     
-     hunpos tagger_fun = (hunpos) malloc(sizeof(value));
+     Hunpos tagger_fun = (Hunpos) malloc(sizeof(value));
 
      // we pass some argument to the function
      CAMLlocalN ( args, 4 );
@@ -68,17 +68,16 @@ hunpos init_hunpos(char* model_file, char* morph_table_file, int max_guessed_tag
      *t =  caml_callbackN( *init_fun, 4, args );
 
      // CAMLreturn1(tagger_fun)
-     CAMLreturnT(hunpos,tagger_fun);
+     CAMLreturnT(Hunpos,tagger_fun);
     
   }
  
-void tag(hunpos hp, int n, char** tokens, char** tags) 
+void hunpos_tagger_tag(Hunpos hp, int n, void* tokens, const char* (*get_token)(void*,int), void* tags, int (*add_tag)(void*,int,const char*), int* error)
 {
   CAMLparam0();
   CAMLlocal3 (return_value,list, v);
   int i;
   list = Val_emptylist;  /* the [] */
-
   // we construct an ocaml string list
   // notice the first token is at the head
   // we spare on List.rev
@@ -86,14 +85,15 @@ void tag(hunpos hp, int n, char** tokens, char** tags)
   {
   	 	 /* Allocate a cons cell */
   		 v = caml_alloc_small(2, 0); 
-  		 Field(v, 0) = caml_copy_string(tokens[i]);  
+//  		 Field(v, 0) = caml_copy_string(mytokens[i]);
+  		 Field(v, 0) = caml_copy_string(get_token(tokens,i));
   		 Field(v, 1) = list; /* add to the list as head */								
   		 list = v;
   }
  
   value* t = hp;
   return_value = caml_callback(*t, list);
-  string_list_to_C_string_array(return_value, tags);
+  string_list_to_C_string_array(return_value, tags, add_tag);
   CAMLreturn0;
   
 }
