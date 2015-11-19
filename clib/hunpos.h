ea831a0737ed7f229204d268ef987d5530b66bbe
Giulio Paci <giuliopaci@gmail.com>
2015-11-19 23:17:14 +0100

    Improved and generalized hunpos.h interface.

--- clib/hunpos.h
+++ clib/hunpos.h
@@ -1,12 +1,33 @@
 #ifndef _HUNPOS_H_
 #define _HUNPOS_H_
 
-typedef  void* hunpos ;
+typedef  void* Hunpos;
+typedef  void* TagList;
 
-hunpos init_hunpos(char* modelfile, char* morphtable, int x, int y);
+/**
+ * @param modelfile
+ * @param morphtable
+ * @param max_guessed_tags
+ * @param theta
+ * @param error pointer to an integer to report errors or NULL
+ *
+ * @return an Hunpos tagger or NULL on errors
+ */
+Hunpos hunpos_tagger_new(const char* modelfile, const char* morphtable, int max_guessed_tags, int theta, int* error);
 
-void tag(hunpos hp, int n, char** tokens, char** tags);
+/**
+ * @param hp
+ * @param n tokens number
+ * @param tokens the tokens to be tagged
+ * @param tags output tags
+ * @param error pointer to an integer to report errors or NULL
+ */
+void hunpos_tagger_tag(Hunpos hp, int n, void* tokens, const char* (*get_token)(void*,int), void* tags, int (*add_tag)(void*,int,const char*), int* error);
 
-
+/**
+ * @param hp
+ * @param error pointer to an integer to report errors or NULL
+ */
+void hunpos_tagger_destroy(Hunpos hp, int* error);
 
 #endif /* _HUNPOS_H_ */
