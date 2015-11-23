7bb6f0e6afdb1265b800f9a04d859c5b626c5220
Simone Daminato <daminato.simone@gmail.com>
2015-11-23 15:46:38 +0100

    Added error checking, fixed invalid return, implemented destruction, fixed indentation.

--- clib/hunposwrap.c
+++ clib/hunposwrap.c
@@ -11,21 +11,22 @@
 
 Hunpos hunpos_tagger_new(const char* model_file, const char* morph_table_file, int max_guessed_tags, int theta, int* error)
 {
-    CAMLparam0();
     if(model_file == NULL) {
+	*error = 3;
 	return NULL;
     }
     if(morph_table_file == NULL) {
 	morph_table_file = "";
     }
-
+    
+    /* Startup OCaml */
+    CAMLparam0();
     char* dummyargv[2];
     dummyargv[0]="";
     dummyargv[1]=NULL;
     caml_startup(dummyargv);
     
-    // notice: ocaml is a functional language
-    // we call a function init that returns a new function which can do the tagging
+    /* get hunpos init function from ocaml */
      static value* init_fun;
      if (init_fun == NULL) 
      {
@@ -41,11 +42,16 @@ Hunpos hunpos_tagger_new(const char* model_file, const char* morph_table_file, i
      args[2] = Val_int(max_guessed_tags);
      args[3] = Val_int(theta);
      
-     // due to the garbage collector we have to register the
-     // returned value not to be deallocated
+     /* due to the garbage collector we have to register the */
+     /* returned value not to be deallocated                 */
      caml_register_global_root(tagger_fun);
      value* t = tagger_fun;
-     *t =  caml_callbackN( *init_fun, 4, args );
+     *t =  caml_callbackN_exn( *init_fun, 4, args );
+     if (Is_exception_result(*t))
+     {
+	*error = 1;
+	CAMLreturnT(Hunpos, NULL);
+     }
 
      // CAMLreturn1(tagger_fun)
      CAMLreturnT(Hunpos,tagger_fun);
@@ -54,33 +60,40 @@ Hunpos hunpos_tagger_new(const char* model_file, const char* morph_table_file, i
  
 void hunpos_tagger_tag(Hunpos hp, int n, void* tokens, const char* (*get_token)(void*,int), void* tags, int (*add_tag)(void*,int,const char*), int* error)
 {
-  CAMLparam0();
-  CAMLlocal3 (return_value,list, v);
-  int i;
-  list = Val_emptylist;  /* the [] */
-  for(i = 0; i< n; i ++)
-  {
-  	 	 /* Allocate a cons cell */
-  		 v = caml_alloc_small(2, 0); 
-  		 Field(v, 0) = caml_copy_string(get_token(tokens,i));
-  		 Field(v, 1) = list; /* add to the list as head */								
-  		 list = v;
-  }
+	CAMLparam0();
+	CAMLlocal3 (return_value,list, v);
+	int i;
+	list = Val_emptylist;  /* the [] */
+	for(i = 0; i< n; i ++)
+	{
+		/* Allocate a cons cell */
+		v = caml_alloc_small(2, 0); 
+		Field(v, 0) = caml_copy_string(get_token(tokens,i));
+		Field(v, 1) = list; /* add to the list as head */								
+		list = v;
+	}
 
-  return_value = caml_callback(*((value*)hp), list);
-  return_value = Field(return_value,1);
+	return_value = caml_callback(*((value*)hp), list);
+	return_value = Field(return_value,1);
 
-  i = 0;
-  while(return_value != Val_emptylist) {
-	  char* s = String_val(Field(return_value, Tag_cons));
-	  add_tag(tags, i++, s);
-	  return_value = Field(return_value, 1);
-  }
+	i = 0;
+	while(return_value != Val_emptylist) {
+		char* s = String_val(Field(return_value, Tag_cons));
+		add_tag(tags, i++, s);
+		return_value = Field(return_value, 1);
+	}
 
-  CAMLreturn0;
+	CAMLreturn0;
   
 }
 
-
+/* hunpos destruction */
+void hunpos_tagger_destroy(Hunpos hp, int* error)
+{
+	CAMLparam0();
+	caml_remove_global_root((value*) hp);
+	free(hp);
+	CAMLreturn0;
+}
 
  
