93a378a5184df9ec13306f3e83754438d235148c
Giulio Paci <giuliopaci@gmail.com>
2015-11-23 11:09:10 +0100

    Fix the behaviour of the tagger.
    Order of tokens matters.
    Observation can be easily ignored in the C wrapper.
    Trying to fix stack overflow.

--- clib/hunposcaml.ml
+++ clib/hunposcaml.ml
@@ -14,14 +14,10 @@ let init model_file morph_table_file  max_guessed_tags theta =
   
   let model = Hmm_tagger.load model_file in
   let tagger = Hmm_tagger.compile_tagger  model hunmorph  max_guessed_tags (log (float_of_int theta)) in
-  let tagger tokens = 
-    (* the first tagger returns (observation, tag) list
-       in this version we simplify everything and returning string list *)
-    let (_, tags) = tagger tokens in
-    
-    tags 
-  in
-  tagger
+    let complete_tagger tokens =
+      let obs, tags = tagger tokens
+      in (List.rev obs, List.rev tags)
+    in complete_tagger
 ;;
 
 let _ = Callback.register "init_from_files" init
