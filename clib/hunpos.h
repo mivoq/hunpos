706935dc8b297d4882183cff1ddf56d42747f351
Simone Daminato <daminato.simone@gmail.com>
2015-12-02 12:55:05 +0100

    Improving error management in hunpos.h

--- clib/hunpos.h
+++ clib/hunpos.h
@@ -19,10 +19,12 @@ Hunpos hunpos_tagger_new(const char* modelfile, const char* morphtable, int max_
  * @param hp
  * @param n tokens number
  * @param tokens the tokens to be tagged
+ * @param get_token function that given @p tokens and an index, returns the token content.
  * @param tags output tags
+ * @param add_tag function that diven @p tags, an index and the tag content, sets the tag content.
  * @param error pointer to an integer to report errors or NULL
  */
-void hunpos_tagger_tag(Hunpos hp, int n, void* tokens, const char* (*get_token)(void*,int), void* tags, int (*add_tag)(void*,int,const char*), int* error);
+void hunpos_tagger_tag(Hunpos hp, int n, void* tokens, const char* (*get_token)(void*,int, int*), void* tags, int (*add_tag)(void*,int,const char*), int* error);
 
 /**
  * @param hp
